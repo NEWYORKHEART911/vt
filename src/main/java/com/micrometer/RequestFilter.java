61692ef3fe9c52aed51b802a91336c09df23e187
NEWYORKHEART911 <123115057+NEWYORKHEART911@users.noreply.github.com>
2025-12-01 20:55:22 -0600

    virtual-threads

---
+++ src/main/java/com/micrometer/RequestFilter.java
@@ -0,0 +1,34 @@
+package com.micrometer;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.io.IOException;
+import java.util.UUID;
+import jakarta.servlet.*;
+
+public class RequestFilter implements Filter {
+
+    private final RequestContext context;
+
+    public RequestFilter(RequestContext context) {
+        this.context = context;
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+            throws IOException, ServletException {
+
+        HttpServletRequest request = (HttpServletRequest) req;
+
+        // These can be added/updated later too
+        context.put("requestPath", request.getRequestURI());
+        context.put("requestId", UUID.randomUUID().toString());
+
+        chain.doFilter(req, res);
+    }
+
+}
