0d159bc99bcd57eeeb9773bc1a6b39fc42b09d39
NEWYORKHEART911 <123115057+NEWYORKHEART911@users.noreply.github.com>
2025-12-04 21:37:26 -0600

    virtual-threads

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -1,7 +1,6 @@
 package com;
 
 import com.xc.ContextAwareExecutor;
-import com.xc.LoggingVirtualThreadExecutor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
