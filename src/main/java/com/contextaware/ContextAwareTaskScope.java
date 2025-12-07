ae990586e5ba3f41a3a7110934a9ae4bcc678f14
NEWYORKHEART911 <123115057+NEWYORKHEART911@users.noreply.github.com>
2025-12-06 21:05:58 -0600

    virtual-threads

--- src/main/java/com/contextaware/ContextAwareTaskScope.java
+++ src/main/java/com/contextaware/ContextAwareTaskScope.java
@@ -3,6 +3,29 @@ package com.contextaware;
 import java.util.concurrent.Callable;
 import java.util.concurrent.StructuredTaskScope;
 
+//just use implements ExecutorService
+//class ContextAwareExecutor implements ExecutorService {
+//
+//    private final ExecutorService delegate;
+//
+//    ContextAwareExecutor(ExecutorService delegate) {
+//        this.delegate = delegate;
+//    }
+//
+//    @Override
+//    public <T> Future<T> submit(Callable<T> task) {
+//        var snapshot = TransactionContext.current().copy();
+//        return delegate.submit(() -> {
+//            ContextHolder.set(snapshot);
+//            try {
+//                return task.call();
+//            } finally {
+//                ContextHolder.clear();
+//            }
+//        });
+//    }
+//}
+
 public final class ContextAwareTaskScope<T> extends StructuredTaskScope<T> {
 
     private final TransactionContext parentContext;
