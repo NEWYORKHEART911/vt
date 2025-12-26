11a8a15d34040e5cc6476766a64284adef03951f
NEWYORKHEART911 <123115057+NEWYORKHEART911@users.noreply.github.com>
2025-12-25 18:47:04 -0600

    virtual-threads

--- checker/src/test/java/com/example/test/TestInput.java
+++ checker/src/test/java/com/example/test/TestInput.java
@@ -2,6 +2,10 @@ package com.example.test;
 
 public class TestInput {
 
+    //The issue is that the compiler plugin
+    // configuration only applies to the main
+    // compilation phase, not the test compilation phase.
+
     static int BAD = 42; // should fail
 
     public static void main(String[] args) {
